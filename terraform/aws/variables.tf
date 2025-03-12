fde8e7fed7fb1b02cafc08c340f5fb126afe4e06
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-03-12 10:36:21 +0100

    func: make simple credential use.

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -14,17 +14,3 @@ variable "project" {
 variable "org" {
   description = "Cycloid org name."
 }
-
-# AWS
-variable "access_key" {
-  description = "AWS IAM access key ID."
-}
-
-variable "secret_key" {
-  description = "AWS IAM access secret key."
-}
-
-variable "aws_region" {
-  description = "AWS region to launch VM."
-  default     = "eu-west-1"
-}
--- terraform/gcp/provider.tf
+++ terraform/gcp/provider.tf
@@ -1,5 +1,3 @@
 provider "google" {
   project = var.gcp_project
-  region  = var.gcp_region
-  zone    = var.gcp_zone
 }
